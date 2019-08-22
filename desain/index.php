603d75ee1e175054027102e5161f42c3f9afeb39
Wahyudi <wahyudi@users.noreply.github.com>
2019-08-22 14:38:36 +0700

    index master

---
+++ desain/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <!-- Site made with Mobirise Website Builder v4.4.0, https://mobirise.com -->
+	  <meta charset="UTF-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta name="generator" content="Mobirise v4.4.0, mobirise.com">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="shortcut icon" href="images/bintang.png" type="image/x-icon">
+	  <meta name="description" content="">
+	  <title>Westudio : Home</title>
+	  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
+	  <link rel="stylesheet" href="w3schools/w3.css">
+	  <link rel="stylesheet" href="css/west-style-navbar.css" type="text/css">
+	  <link rel="stylesheet" href="css/west-footer.css">
+	  <link rel="stylesheet" href="css/west-header.css">
+	  <link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="css/transisi.css">
+	  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	  <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+	  <style>
+			/* Underline From Center */
+			.hvr-underline-from-center {
+			  display: inline-block;
+			  vertical-align: middle;
+			  -webkit-transform: perspective(1px) translateZ(0);
+			  transform: perspective(1px) translateZ(0);
+			  box-shadow: 0 0 1px transparent;
+			  position: relative;
+			  overflow: hidden;
+				}
+			.hvr-underline-from-center:before {
+			  content: "";
+			  position: absolute;
+			  z-index: -1;
+			  left: 50%;
+			  right: 50%;
+			  bottom: 0;
+			  background:red;
+			  height: 2px;
+			  -webkit-transition-property: left, right;
+			  transition-property: left, right;
+			  -webkit-transition-duration: 0.3s;
+			  transition-duration: 0.3s;
+			  -webkit-transition-timing-function: ease-out;
+			  transition-timing-function: ease-out;
+				}
+			.hvr-underline-from-center:hover:before, .hvr-underline-from-center:focus:before, .hvr-underline-from-center:active:before {
+			  left: 0;
+			  right: 0;
+				}  
+			.ubahwarna{
+				}
+			.ubahwarna:hover{
+				color:red;
+			}
+			
+			.ubahwarna2{
+				-webkit-transition: width 1s, height 1s; /* For Safari 3.1 to 6.0 */
+				transition: width 1s, height 1s;
+				}
+			.ubahwarna2:hover{
+				color:yellow;
+				}
+
+			.hue{
+			}
+			.hue:hover{
+				filter:hue(90deg);
+			}
+			</style>
+	</head>
+<body>
+	<!--Navigasi Menu
+	<section class="menu cid-qB0W3W0u00" once="menu" id="menu2-2" data-rv-view="563">-->
+	<section class="west-navbar">
+		<nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<div class="hamburger">
+				 </div>
+			</button>
+			<div class="menu-logo">
+				<div class="navbar-brand">
+					<span class="navbar-logo">
+						<a href="index.php?go=Home">
+							<img src="images/westudio1-826x225.png" alt="Westudio Design" title="Westudio Design" media-simple="true" style="height: 5.3rem;">
+						</a>
+					</span>
+					
+				</div>
+			</div>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+					<li class="nav-item">
+						<a class="nav-link link text-black display-4 hvr-underline-from-center " href="index.php?go=Home"><span class="ubahwarna">Home</span></a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link link text-black display-4 hvr-underline-from-center " href="index.php?go=Service"><span class="ubahwarna">Services</span></a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link link text-black display-4 hvr-underline-from-center " href="index.php?go=Portofolio"><span class="ubahwarna">Portofolio</span></a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link link text-black display-4 hvr-underline-from-center" href="index.php?go=About"><span class="ubahwarna">About Us</span></a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link link text-black display-4 hvr-underline-from-center" href="index.php?go=Contact"><span class="ubahwarna">Contact</span></a>
+						</li>
+					<li class="nav-item">
+						<a class="nav-link link text-black display-4 hvr-underline-from-center" href="index.php?go=Order"><span class="ubahwarna">Order</span></a>
+						</li>
+					</ul>
+				<a button class=" navbar-right btn btn-info " style="border-radius: 30px;" href="tel:+6287731336696">
+						<i class="fa fa-phone"></i>
+						<span class="ubahwarna2">+62-877-3133-6696</span>
+						
+					</a></div>
+			</div>
+		</nav>
+	</section>
+<!--End Navigasi Menu-->
+
+
+<?php include "page.php" ?>
+
+
+<!--Footer-->
+<!--<section once="" style="background-color:#1A1A1A;" class="cid-qB184cVbhP mbr-reveal" id="footer7-c" data-rv-view="568">
+-->
+	<section once="" style="background-color:#1A1A1A;" class="mbr-reveal">
+	 
+	 
+	<?php include "footer.php" ?>
+	</section>
+
+  <script src="web/jquery/jquery.min.js"></script>
+  <script src="popper/popper.min.js"></script>
+  <script src="tether/tether.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="smooth-scroll/smooth-scroll.js"></script>
+  <!--<script src="cookies-alert-plugin/cookies-alert-core.js"></script>
+  <script src="cookies-alert-plugin/cookies-alert-script.js"></script>-->
+  <script src="touch-swipe/jquery.touch-swipe.min.js"></script>
+  <script src="viewport-checker/jquery.viewportchecker.js"></script>
+  <script src="jarallax/jarallax.min.js"></script>
+  <script src="dropdown/js/script.min.js"></script>
+  <script src="theme/js/script.js"></script>
+  
+  <input name="animation" type="hidden">
+  <!--<input name="cookieData" type="hidden" data-cookie-text="We use cookies to give you the best experience. 
+  Read our <a href='privacy.html'>cookie policy</a>.">-->
+  </body>
+</html>
